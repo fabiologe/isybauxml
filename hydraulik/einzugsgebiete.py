613058d208ba5b88047a2c8370cb9755959aba97
fabiologe <128411552+fabiologe@users.noreply.github.com>
2024-01-10 16:56:56 +0100

    Add files via upload

--- hydraulik/einzugsgebiete.py
+++ hydraulik/einzugsgebiete.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 einzugsgebiete_list = []
 @dataclass
-class einzugsgebiete:
+class inzugsgebiete:
     gebietskennung: Optional[str] = None
     gebietsname: Optional[str] = None
     kommentar: Optional[str] = None
@@ -11,4 +11,27 @@ class einzugsgebiete:
     einwohnerdichte: Optional[float] = None # E/hages
     trockenwetterkennung: Optional[str] = None
 
-
+def parse_einzugsgebiete(root):
+    # Extract the data into custom classes
+    for einzugsgebiet_objekt in root.getElementsByTagName('Einzugsgebiet'):
+        einzugsgebiet = einzugsgebiete()
+        gebietskennung_element = einzugsgebiet_objekt.getElementsByTagName('Gebietskennung')
+        if gebietskennung_element:
+            einzugsgebiet.gebietskennung = str(gebietskennung_element[0].firstChild.nodeValue)
+        gebietsname_element = einzugsgebiet_objekt.getElementsByTagName('Gebietsname')
+        if gebietsname_element:
+            einzugsgebiet.gebietsname = str(gebietsname_element[0].firstChild.nodeValue)
+        kommentart_element = einzugsgebiet_objekt.getElementsByTagName('Kommentar')
+        if kommentart_element: 
+            einzugsgebiet.kommentar = str(einzugsgebiet_objekt[0].firstChild.nodeValue)
+        einwohnerwerte_element = einzugsgebiet_objekt.getElementsByTagName('Einwohnerwerte')
+        if einwohnerwerte_element:
+            einzugsgebiet.Einwohnerwerte = float(einzugsgebiet_objekt[0].firstChild.nodeValue)
+        einwohnerdichte_element = einzugsgebiet_objekt.getElementsByTagName('Einwohnerdichte')
+        if einwohnerdichte_element:
+            einzugsgebiet.einwohnerdichte = float(einzugsgebiet_objekt[0].firstChild.nodeValue)
+        trockenwetterkennung_element = einzugsgebiet_objekt.getElementsByTagName('Trockenwetterkennung')
+        if trockenwetterkennung_element:
+            einzugsgebiet.trockenwetterkennung = str(einzugsgebiet_objekt[0].firstChild.nodeValue)
+        
+        einzugsgebiete_list.append(einzugsgebiet)
